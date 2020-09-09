a4db7507e678f56c034d641352135c17290aea8c
Carlos Vicente <carlosvchrdevelop@gmail.com>
2020-09-09 14:03:24 +0200

    Docs agregado

---
+++ docs/precache-manifest.34377b03296fb6d3a34db5c0ade55d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2944dcd5a8318f82b0e8e8dee7fa2795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "348c2d191035de080123",
+    "url": "/static/css/main.a9db9cf4.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348c2d191035de080123",
+    "url": "/static/js/main.b63173ad.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
